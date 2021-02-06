565a35793091103796527d0dc4d539bfa7de0d9a
Akram Sabbah <akramsabbah9@gmail.com>
2021-02-06 12:09:07 -0800

    imported packages and got inquirer & writeFile working

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
-// TODO: Include packages needed for this application
+// packages: fs, inquirer, and generateMarkdown function from utils.
+// REMEMBER TO ACTUALLY INSTALL INQUIRER BEFORE USING THIS APP!
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "username",
+        message: "Enter your GitHub username (Required)",
+        validate: input => {
+            if (input) return true;
+            else return console.log("Please enter your username!"); // falsy, so this works!
+        }
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "Enter the title of your README (Required)",
+        validate: input => {
+            if (input) return true;
+            else return console.log("Please enter the title!"); // falsy, so this works!
+        }
+    },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// generate markdown from the data and write it to README file
+const writeToFile = (fileName, data) => {
+    return new Promise((resolve, reject) => {
+        // make a readme string from the data
+        const readme = generateMarkdown(data);
+        
+        fs.writeFile(fileName, readme, err => {
+            // if there's an error writing the file, reject the Promise.
+            if (err) return reject(err);
 
-// TODO: Create a function to initialize app
-function init() {}
+            //otherwise, resolve as normal.
+            resolve({
+                ok: true,
+                message: "File created!"
+            });
+        });
+    });
+};
 
-// Function call to initialize app
+// initialize app: prompt user for README content, then write to file.
+const init = () => {
+    inquirer.prompt(questions)
+    .then(data => writeToFile("./dist/README.md", data))
+    .catch(err => console.log(err));
+};
+
+// initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,11 +10,11 @@ function renderLicenseLink(license) {}
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
+// generate markdown for README
+const generateMarkdown = data => {
     return `# ${data.title}
 
 `;
-}
+};
 
 module.exports = generateMarkdown;
